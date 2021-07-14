e02799643e87144f82acce0fc4ff82ff61151d54
hquach729 <hquach729@icloud.com>
2021-07-13 17:42:23 -0700

    change App.js text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
 					target='_blank'
 					rel='noopener noreferrer'
 				>
-					Learn React: Docker and Kubenetes Version 4
+					I was changed on the Feature Branch
 				</a>
 			</header>
 		</div>
